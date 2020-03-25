6ff57306ae31921777f8d1ef607bfc0a3102961a
potatoe-potatoe <aprilgvguevara@gmail.com>
2020-03-25 13:25:51 +0800

    [MISC] Clean-up FormComponent.js

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -3,10 +3,8 @@ import * as MUI from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import phLocations from '../json/parsedLocations.json';
 import ppeList from '../json/ppeList.json';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
 
-function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitChange, handleListChange, changeBrgyList, ...props }) {
+function Form({ styles, handleCustomSubmit, handleFormInputChange, handleSubmitChange, handleListChange, changeBrgyList, ...props }) {
   const { 
     values: {
       type, 
@@ -27,18 +25,18 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
     setFieldValue
   } = props;
 
-  console.log('--------- START ---------');
-  console.log('This is initial touched: ', touched);
-  console.log('This is errors: ', errors);
+  // console.log('--------- START ---------');
+  // console.log('This is initial touched: ', touched);
+  // console.log('This is errors: ', errors);
 
-  for (let error in errors) {
-    console.log(`This is [${error}] error: `, errors[error]);
-  }
+  // for (let error in errors) {
+  //   console.log(`This is [${error}] error: `, errors[error]);
+  // }
 
   function isTouched() {
-    console.log('This is the array: ', touched);
+    // console.log('This is the array: ', touched);
     for (let field in touched) {
-      console.log('This is current: ', touched[field]);
+      // console.log('This is current: ', touched[field]);
 
       if (!touched[field]) {
         return false;
@@ -48,28 +46,21 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
     return true;
   }
 
-  function handleTempSubmit(e) {
-    e.preventDefault();
-    console.log('Values are: ', props.values);
-  }
-
-  function handleActualSubmit(e) {
+  function handleFormSubmit(e) {
     e.preventDefault();
 
     handleSubmitChange(props.handleSubmit(e));
 
-    alert("submit kek");
-    alert('look at errors: ', errors);
+    // alert("submit kek");
+    alert('look at errors: ', JSON.stringify(errors, null, 2));
+    alert(JSON.stringify(props.values, null, 2));
 
     if (isValid && isTouched()) {
       // stuff here
     }
-
-    alert(JSON.stringify(props.values, null, 2));
   }
 
   const handleInputChange = (key, event) => {
-    console.log('Handling change for event: ', event);
     event.persist();
 
     props.handleChange(event);
@@ -80,26 +71,23 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
     if (key === 'locCity') {
       const list = changeBrgyList(city);
       setFieldValue('brgyList', list)
-
     }
 
     setTimeout(() => console.log('Value of event: ', eval(key)), 2000);
   }
 
-  console.log('isTouched: ', (isTouched()));
-  console.log('!isTouched: ', !(isTouched()));
-  console.log('!isValid: ', !isValid);
-  console.log('Print this: ', (!isValid || !(isTouched())));
+  // console.log('isTouched: ', (isTouched()));
+  // console.log('!isTouched: ', !(isTouched()));
+  // console.log('!isValid: ', !isValid);
+  // console.log('Print this: ', (!isValid || !(isTouched())));
 
   return (
     <React.Fragment>
       <MUI.Paper style={{padding: '30px 12px'}}>
         <MUI.Container>
-          <form onSubmit={handleActualSubmit}>
+          <form onSubmit={handleFormSubmit}>
             <MUI.FormGroup>
-              <MUI.FormControl 
-                error={Boolean(errors.type)}
-              >
+              <MUI.FormControl error={Boolean(errors.type)}>
                 <MUI.RadioGroup
                   className={styles.formGroupMargin}
                   name="type"
@@ -109,10 +97,12 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                   <MUI.FormLabel className={styles.formTypographyPadding} component="legend">
                       Are you <b>in need of donations</b> or <b>offering donations</b>?
                   </MUI.FormLabel>
+
                   <MUI.FormControlLabel
                     value="need"
                     control={<MUI.Radio color="primary" />}
                     label="I am in need of donations." />
+
                   <MUI.FormControlLabel 
                     value="offer"
                     control={<MUI.Radio color="primary" />}
@@ -135,7 +125,6 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                       <MUI.Select
                         name="item"
                         value={item}
-                        label="item"
                         onChange={handleInputChange.bind(null, 'item')}
                       >
                         <MUI.MenuItem disabled value='default'>
@@ -156,12 +145,12 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                   <MUI.Grid item xs={3}>
                     <MUI.FormControl fullWidth>
                       <MUI.TextField
-                        type="number"
-                        value={amount}
                         name="amount"
-                        helperText={Boolean(errors.amount) ? errors.amount : ""}
-                        error={Boolean(errors.amount)}
+                        type="number"
                         label="Amount"
+                        value={amount}
+                        error={Boolean(errors.amount)}
+                        helperText={Boolean(errors.amount) ? errors.amount : ""}
                         InputLabelProps={{ shrink: true }}
                         onChange={handleInputChange.bind(null, 'amount')} />
                     </MUI.FormControl>
@@ -190,13 +179,13 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                           <MUI.TextField
                             {...params}
                             name="locCity"
-                            helperText={Boolean(errors.locCity) ? errors.locCity : ""}
-                            error={Boolean(errors.locCity)}
+                            label="City / Municipality"
                             value={locCity}
+                            error={Boolean(errors.locCity)}
+                            helperText={Boolean(errors.locCity) ? errors.locCity : ""}
+                            placeholder="--- Select City ---"
                             InputLabelProps={{ shrink: true }}
                             InputProps={{ ...params.InputProps }}
-                            label="City / Municipality"
-                            placeholder="--- Select City ---"
                             onBlur={handleInputChange.bind(null, 'locCity')} />
                         )} />
                     </MUI.FormControl>
@@ -205,8 +194,8 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                   <MUI.Grid item xs={12} md={4} alignItems="center">
                     <MUI.FormControl fullWidth>
                       <Autocomplete
-                        options={brgyList}
                         autoHighlight
+                        options={brgyList}
                         renderOption={option => (
                           <React.Fragment>
                             {option}
@@ -216,13 +205,13 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                           <MUI.TextField
                             {...params}
                             name="locBarangay"
-                            helperText={Boolean(errors.locBarangay) ? errors.locBarangay : ""}
-                            error={Boolean(errors.locBarangay)}
+                            label="Barangay"
                             value={locBarangay}
+                            error={Boolean(errors.locBarangay)}
+                            helperText={Boolean(errors.locBarangay) ? errors.locBarangay : ""}
+                            placeholder="--- Select Barangay ---"
                             InputLabelProps={{ shrink: true }}
                             InputProps={{ ...params.InputProps }}
-                            label="Barangay"
-                            placeholder="--- Select Barangay ---"
                             onChange={handleInputChange.bind(null, 'locBarangay')} />
                         )} />
                     </MUI.FormControl>
@@ -232,12 +221,10 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                     <MUI.FormControl fullWidth>
                       <MUI.TextField
                         name="locOther"
-                        helperText={Boolean(errors.locOther) ? errors.locOther : ""}
-                        error={Boolean(errors.locOther)}
-                        value={locOther}
-                        InputLabelProps={{ shrink: true }}
                         label="Other Location Details"
+                        value={locOther}
                         placeholder="Example: street number, block, area"
+                        InputLabelProps={{ shrink: true }}
                         onChange={handleInputChange.bind(null, 'locOther')} />
                     </MUI.FormControl>
                   </MUI.Grid>
@@ -254,12 +241,12 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                     <MUI.FormControl fullWidth>
                       <MUI.TextField
                         name="contactPerson"
-                        helperText={Boolean(errors.contactPerson) ? errors.contactPerson : ""}
-                        error={Boolean(errors.contactPerson)}
-                        value={contactPerson}
-                        InputLabelProps={{ shrink: true }}
                         label="Contact Person"
+                        value={contactPerson}
+                        error={Boolean(errors.contactPerson)}
+                        helperText={Boolean(errors.contactPerson) ? errors.contactPerson : ""}
                         placeholder="Example: Juan dela Cruz"
+                        InputLabelProps={{ shrink: true }}
                         onChange={handleInputChange.bind(null, 'contactPerson')} />
                     </MUI.FormControl>
                   </MUI.Grid>
@@ -268,12 +255,12 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                     <MUI.FormControl fullWidth>
                       <MUI.TextField
                         name="contactNumber"
-                        helperText={Boolean(errors.contactNumber) ? errors.contactNumber : ""}
-                        error={Boolean(errors.contactNumber)}
-                        value={contactNumber}
-                        InputLabelProps={{ shrink: true }}
                         label="Contact #"
+                        value={contactNumber}
+                        error={Boolean(errors.contactNumber)}
+                        helperText={Boolean(errors.contactNumber) ? errors.contactNumber : ""}
                         placeholder="Insert mobile or landline number"
+                        InputLabelProps={{ shrink: true }}
                         onChange={handleInputChange.bind(null, 'contactNumber')} />
                     </MUI.FormControl>
                   </MUI.Grid>
@@ -281,14 +268,14 @@ function Form({ styles, handleFormSubmit, handleFormInputChange, handleSubmitCha
                   <MUI.Grid item xs={12} sm={6}>
                     <MUI.FormControl fullWidth>
                       <MUI.TextField
-                      name="contactFacebook"
-                      helperText={Boolean(errors.contactFacebook) ? errors.contactFacebook : ""}
-                      error={Boolean(errors.contactFacebook)}
-                      value={contactFacebook}
-                          InputLabelProps={{ shrink: true }}
-                          label="Facebook"
-                          placeholder="Insert name or link"
-                          onChange={handleInputChange.bind(null, 'contactFacebook')} />
+                        name="contactFacebook"
+                        label="Facebook"
+                        value={contactFacebook}
+                        error={Boolean(errors.contactFacebook)}
+                        helperText={Boolean(errors.contactFacebook) ? errors.contactFacebook : ""}
+                        placeholder="Insert name or link"
+                        InputLabelProps={{ shrink: true }}
+                        onChange={handleInputChange.bind(null, 'contactFacebook')} />
                     </MUI.FormControl>
                   </MUI.Grid>
                 </MUI.Grid>
